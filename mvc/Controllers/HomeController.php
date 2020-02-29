7cff651053c5686cce4ff4523adc675b65f325b1
Rafael <rafaelsenaferreira@hotmail.com>
2020-02-29 20:06:57 -0300

    Sistema MVC

---
+++ mvc/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+	
+	namespace Controllers;
+
+	class HomeController
+	{
+
+		public function __construct(){
+			$this->view = new \Views\MainView('home');
+		}
+		
+		public function executar(){
+			$this->view->render(array('titulo'=>'Home'));
+		}
+	}
+?>
